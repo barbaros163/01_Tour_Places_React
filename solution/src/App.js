11ddff10d5d4384aeb21830c3ed27693dbdc4db9
Yasin Yavuz <barbaros1675@gmail.com>
2022-11-30 14:42:15 +0100

    solution start

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,16 +1,13 @@
-import './App.scss';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Navbar from './components/Navbar/Navbar';
+import "./App.scss";
 
 const App = () => {
-  return(
+  return (
     <div className="App">
-      <Navbar/>
-      <Header/>
-      <Main/>
+      <Navbar />
+      <Header />
+      <Main />
     </div>
-  )
-}
+  );
+};
 
 export default App;
